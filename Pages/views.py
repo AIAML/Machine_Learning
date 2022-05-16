6ce0449e161ec273a7a1b0df48c161a207a39fc7
majid.hemmati@yahoo.com <!+Karaj1985>
2022-05-16 23:04:54 +0430

    j

---
+++ Pages/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def home_view(request,*args,**kwargs):
+    #my_context = {"my_text":"This is main page","my_number":134}
+    return render(request,"home.html", {})
