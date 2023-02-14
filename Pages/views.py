4d2853b71ba72a376e5f9866c2fe559ab9f9fa7d
Majid Hemmati <majid.hemmati@yahoo.com>
2023-02-14 08:42:24 +0330

    machine learning for everyone

--- Pages/views.py
+++ Pages/views.py
@@ -6,5 +6,15 @@ def home_view(request,*args,**kwargs):
     return render(request,"home.html", {})
 
 def supervisedlearning_view(request,*args,**kwargs):
-    #my_context = {"my_text":"This is main page","my_number":134}
-    return render(request,"supervisedlearning.html", {})
+    my_context = {"my_methods":["Nearest Neighbors",
+    "Linear SVM",
+    "RBF SVM",
+    "Gaussian Process",
+    "Decision Tree",
+    "Random Forest",
+    "Neural Net",
+    "AdaBoost",
+    "Naive Bayes",
+    "QDA"]}
+
+    return render(request,"supervisedlearning.html", my_context)
