d99fcb6422d8cf8eaa90b96000eb5901e46441a8
Majid Hemmati <majid.hemmati@yahoo.com>
2023-02-15 08:35:51 +0330

    machine learning for everyone

--- SupervisedLearning/urls.py
+++ SupervisedLearning/urls.py
@@ -1,4 +1,5 @@
-from SupervisedLearning.views import supervisedlearning_view
+from SupervisedLearning.views import supervisedlearning_view,knn_view
 from django.urls import include, path,include
 urlpatterns = [
-path('', supervisedlearning_view, ''),]
+path('', supervisedlearning_view, ''),
+path('knn/', knn_view, ''),]
--- SupervisedLearning/views.py
+++ SupervisedLearning/views.py
@@ -14,5 +14,18 @@ def supervisedlearning_view(request, *args, **kwargs):
 
     return render(request, "homeSL.html", my_context)
 
+def knn_view(request, *args, **kwargs):
+    my_context = {"my_methods": ["Nearest Neighbors",
+                                 "Linear SVM",
+                                 "RBF SVM",
+                                 "Gaussian Process",
+                                 "Decision Tree",
+                                 "Random Forest",
+                                 "Neural Net",
+                                 "AdaBoost",
+                                 "Naive Bayes",
+                                 "QDA"]}
+
+    return render(request, "knn.html", my_context)
 
 
