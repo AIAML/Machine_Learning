52cbead68f774a9ed48207cacc08b76cb8db2248
Majid Hemmati <majid.hemmati@yahoo.com>
2023-02-18 22:05:31 +0330

    machine learning for everyone

--- SupervisedLearning/views.py
+++ SupervisedLearning/views.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 from sklearn.datasets import make_blobs
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
-
+import matplotlib.cm as cm
+import sys
 def supervisedlearning_view(request, *args, **kwargs):
     my_context = {"my_methods": ["Nearest Neighbors",
                                  "Linear SVM",
@@ -24,11 +25,34 @@ def supervisedlearning_view(request, *args, **kwargs):
 
 def knn_view(request, *args, **kwargs):
     my_context = {"my_methods": ["Nearest Neighbors"]}
-    X, y = make_blobs(n_samples=800, n_features=2, centers=9, cluster_std=1.5, random_state=4)
-    plt.style.use('seaborn')
-    plt.figure(figsize=(10, 10))
-    plt.scatter(X[:, 0], X[:, 1], c=y, marker='*', s=100, edgecolors='black')
+
+    x = [6, 7, 11, 6, 4, 12, 15, 9, 11, 13]
+    y = [22, 29, 23, 17, 18, 26, 25, 23, 22, 23]
+    classes = [0, 0, 1, 0, 0, 1, 1, 0, 1, 1]
+
+    plt.scatter(x , y , c=classes )
+
     plt.show()
+
+
+    data = list(zip(x, y))
+    knn = KNeighborsClassifier(n_neighbors=5)
+
+    knn.fit(data, classes)
+
+    new_x = 8
+    new_y = 21
+    new_point = [(new_x, new_y)]
+
+    prediction = knn.predict(new_point)
+
+    plt.scatter(x + [new_x], y + [new_y], c=classes + [prediction[0]])
+    plt.text(x=new_x - 1.7, y=new_y - 0.7, s=f"new point, class: {prediction[0]}")
+    plt.show()
+
+    # Two  lines to make our compiler able to draw:
+    plt.savefig("d://abc.jpg")
+
 
     return render(request, "knn.html", my_context)
 
